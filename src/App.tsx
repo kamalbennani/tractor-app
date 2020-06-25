42b78d4a8e93e3806ae9cca5b804a8bd25c9e66c
Kamal Bennani Bouchiba <kamalbouchboy1@hotmail.fr>
2020-06-25 09:22:19 +0000

    implement todo buttons and title

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,36 @@
-import React from 'react';
-import { Button, ThemeProvider } from '@aircall/tractor';
+import React from "react";
+import {
+  Button,
+  ThemeProvider,
+  Typography,
+  Box,
+  Spacer,
+  ArchiveOutlined,
+  ChevronDownOutlined,
+} from "@aircall/tractor";
 
-import { AppLayout } from './components/AppLayout';
+import { AppLayout } from "./components/AppLayout";
 
 function App() {
   return (
     <ThemeProvider>
-        <AppLayout>
-            <Button>Delete</Button>
-        </AppLayout>
+      <AppLayout>
+        <Box padding="14px 16px 13px">
+          <Spacer justifyContent="space-between" alignItems="center" fluid>
+            <Typography variant="displayM" weight="bold">
+              To-do
+            </Typography>
+            <Spacer>
+              <Button mode="link" size="small">
+                Archive All <ArchiveOutlined />
+              </Button>
+              <Button mode="link" size="small">
+                All numbers <ChevronDownOutlined />
+              </Button>
+            </Spacer>
+          </Spacer>
+        </Box>
+      </AppLayout>
     </ThemeProvider>
   );
 }
